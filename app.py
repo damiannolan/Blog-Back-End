291a817367d75a141672af7c1de681a61592dbf5
seantking <sean7thomasking@gmail.com>
2016-10-09 10:04:55 +0100

    returning dict correctly - bson library imported

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask
 import pymongo
 from flask_cors import CORS, cross_origin
+from bson.json_util import dumps
 
 app = Flask(__name__)
 CORS(app)
@@ -9,11 +10,10 @@ CORS(app)
 client = pymongo.MongoClient()
 db = client.test
 
-
 @app.route('/', methods=['GET'])
 def get_tasks():
-	task = db.test_form.find_one()
-	return task
+	task = db['test_form'].find_one()
+	return dumps(task) 
 
 if __name__ == '__main__':
     app.run(debug=True)
