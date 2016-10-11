c093707af0114e2e8efef4b7541bd15f61969813
seantking <sean7thomasking@gmail.com>
2016-10-11 16:08:58 +0100

    minor fix

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, request
 import pymongo
 from flask_cors import CORS, cross_origin
 from bson.json_util import dumps
@@ -10,10 +10,20 @@ CORS(app)
 client = pymongo.MongoClient()
 db = client.test
 
-@app.route('/', methods=['GET'])
-def get_tasks():
-	task = db['test_form'].find_one()
-	return dumps(task) 
+@app.route('/tasks', methods=['GET', 'POST'])
+def tasks():
+	if request.method == 'POST':
+		if request.headers['Content-Type'] == 'application/json':
+			task = request.get_json()
+			db.test_form.insert_one(task)
+			return 'Posted' 
+
+	elif request.method == 'GET': 
+		task = db['test_form'].find_one()
+		return dumps(task) 
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    #curl -i -H "Content-Type: application/json" -X POST -d '{"id":5, "title": "lewl", "description": "hahahha", "done": false}' http://localhost:5000/tasks
